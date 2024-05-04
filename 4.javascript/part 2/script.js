4ac0fb4f0cf16d33520e284e916cc4632adcfab2
nadeemshariff <108820002+nadeemshariff@users.noreply.github.com>
2024-05-04 16:52:10 +0530

    ..

---
+++ 4.javascript/part 2/script.js
@@ -0,0 +1,25 @@
+// 'use strict';  // eslint-disable-line strict
+/* global describe, it */   
+
+// let license = false;
+// const test = true;
+
+// if (test) license = true; {
+//     if(license) console.log('DRIVE!!!!!!! :D');
+// }
+
+// const interface = 'Audio';
+// const private = 53468;
+
+// function logger() {
+//     console.log('Looged')
+// }
+// // calling or runnig or invoking the function
+// logger();
+
+function fruitProcessor(apples, oranges)  {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;// this becomes result of the function
+}
+const output1 = fruitProcessor(2, 4);
+console.log(output1);
---
+++ 4.javascript/script.js
@@ -0,0 +1,78 @@
+// Rating mini app
+
+// let rating = 5;
+
+// if (rating == 5) {
+//     console.log('5 stars');
+// } else if (rating == 4) {
+//     console.log('4 stars');
+// } else if (rating == 3) {
+//     console.log('3 stars');
+// } else if (rating == 2) {
+//     console.log('2 stars');
+// } else if (rating == 1) {
+//     console.log('1 star');
+// } else {
+//     console.log('invalid rating');
+    
+// }
+
+// Switch statement
+
+// let user = 'Mentor';
+
+// switch (user) {
+//     case "Admin":
+//         console.log('Redirect to Admin Page');
+//         break;
+//         case "Mentor":
+//             console.log('Redirect to Mentor Page');
+//             break;
+
+//     default:
+//         break;
+// }
+
+// Ternary Operator
+
+// condition ? true: false
+
+// const user = document.getElementById('user');
+// if(user === null){
+//     alert("User not found");
+// }else{
+//     const message = user.value;
+//     document.write(`Hello, ${message}`);
+//     let userInput = prompt("Enter a value:"); // Prompt the user for input
+//     let condition = userInput ? true : false; // Define the condition based on user input
+//     console.log(condition); // Output the condition
+// }
+
+
+
+// const user = document.getElementById('user');
+// if(user === null){
+//     alert("User not found");
+// }else{
+//     const message = user.value;
+//     document.write(`Hello, ${message}`);
+// }
+// let r = user;
+
+// r ? console.log('Its fuckng true ') : console.log('Its fuckng False');
+
+// const now = new Date();
+// console.log(now.toString());
+
+// console.log(now.getFullYear());
+// console.log(now.getTime());
+
+// let x,y;
+// x = y = 25 -10-5;
+// console.log(x,y);
+
+// const avg = x + y/2;
+// console.log(x,y,avg);
+
+// ----------------------------//
+
